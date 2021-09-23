e1b59e57a0b0e1c8ea73e196ea0656f890f84b96
yousraRB <yousrarebai886@gmail.com>
2021-09-23 18:49:02 +0200

    java1

---
+++ les classe/lab1/src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, me");
+        String a = "salam";
+        System.out.println(a);
+        int b = 5;
+        double c = 3;
+        System.out.println("b=" + b + "c=" + c);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("atini valeur");
+        int d = sc.nextInt();
+        System.out.println("la variable t3k is :" + d);
+
+    }
+}
